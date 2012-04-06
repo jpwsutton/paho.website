93edddaeab562efb0ffd7c6628e144bc3cc544a7
sdedeugd <sdedeugd>
2012-04-06 23:38:19 +0000

    new documantation  page format

--- _projectCommon.php
+++ _projectCommon.php
@@ -8,33 +8,33 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    
+ *
  *******************************************************************************/
 
 	# Set the theme for your project's web pages.
 	# See http://eclipse.org/phoenix/
 	$theme = "Nova";
-	
+
 
 	# Define your project-wide Navigation here
 	# This appears on the left of the page if you define a left nav
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
-	
-	# If you want to override the eclipse.org navigation, uncomment below.
-	# $Nav->setLinkList(array());
-	
-	# Break the navigation into sections
-	$Nav->addNavSeparator("MyProject", 	"/default");
-	$Nav->addCustomNav("Download", "/project/download.php", "_self", 3);
-	$Nav->addCustomNav("Documentation", "/project/documentation.php", "_blank", 3);
-	$Nav->addCustomNav("Support", "/project/support.php", "_blank", 3);
-	$Nav->addCustomNav("Getting Involved", "/project/developers", "_blank", 3);
 
+	# If you want to override the eclipse.org navigation, uncomment below.
+ 	# $Nav->setLinkList(array());
+
+	# Break the navigation into sections
+	$Nav->addNavSeparator("Paho", 	"/project");
+	$Nav->addCustomNav("Download", "/project/download.php", "_self", 3);
+	$Nav->addCustomNav("Documentation", "/project/documentation", "_blank", 3);
+	$Nav->addCustomNav("Support", "/project/support", "_blank", 3);
+	$Nav->addCustomNav("Getting Involved", "/project/developers", "_blank", 3);
+	
 	# Define keywords, author and title here, or in each PHP page specifically
-	$pageKeywords	= "xtext, dsl, modeling, domain specific language, textual";
-	$pageAuthor		= "Your name";
-	# $pageTitle 		= "Xtext";
+	$pageKeywords	= "paho, m2m, Machine-to-Machine, Internet of Things, IoT";
+	$pageAuthor		= "Scott de Deugd";
+	# $pageTitle 		= "Paho";
 
 
 	# top navigation bar
@@ -45,13 +45,17 @@
 	# $Menu->addMenuItem("Documentation", "/project/documentation.php", "_self");
 	# $Menu->addMenuItem("Support", "/project/support.php", "_self");
 	# $Menu->addMenuItem("Developers", "/project/developers", "_self");
-	
+
 	# To define additional CSS or other pre-body headers
-	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/default/style.css"/>');
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/paho/style.css"/>'."\n");
 	
+	# Following link needed to add Paho header to all pages
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style.css"/>'."\n");
+	
+
 	# To enable occasional Eclipse Foundation Promotion banners on your pages (EclipseCon, etc)
 	$App->Promotion = TRUE;
-	
+
 	# If you have Google Analytics code, use it here
-	# $App->SetGoogleAnalyticsTrackingCode("YOUR_CODE");
+	$App->SetGoogleAnalyticsTrackingCode("UA-594061-9");
 ?>

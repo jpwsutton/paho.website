2c4ad9e1126cd944a2f3a1aaa6fc2bd3d93cd3a8
Ian Craggs <icraggs@uk.ibm.com>
2015-05-12 14:04:05 +0100

    Update embedded clients page

--- clients/c/embedded/index.php
+++ clients/c/embedded/index.php
@@ -1,9 +1,9 @@
 <?php include '../../../_includes/header.php' ?>
 
-<h1>Embedded MQTT C/C++ Client</h1>
+<h1>Embedded MQTT C/C++ Client Libraries</h1>
 
 <p>The "full" Paho MQTT C client library was written with Linux and Windows in mind.  It assumes the existence of
-Posix or Windows libraries for networking (sockets), threads and memory allocation. This library is intended to have these characteristics:
+Posix or Windows libraries for networking (sockets), threads and memory allocation. These libraries are intended to have these characteristics:
 </p>
 
 <ul>
@@ -13,7 +13,18 @@ Posix or Windows libraries for networking (sockets), threads and memory allocati
 <li>optional higher layer(s) in C and/or C++.</li>
 </ul>
 
-<p>The library can be used on desktop operating systems, but is primarily aimed for environments such as <a href="http://mbed.org">mbed</a>, <a href="http://www.arduino.cc/">Arduino</a> and <a href="http://freertos.org">FreeRTOS</a>.
+<p>The libraries can be used on desktop operating systems, but is primarily aimed for environments such as <a href="http://mbed.org">mbed</a>, <a href="http://www.arduino.cc/">Arduino</a> and <a href="http://freertos.org">FreeRTOS</a>.
+</p>
+
+<p>Currently, the libraries that exist are:
+<ul>
+<li>MQTTPacket.  This is the lowest level library, the simplest and smallest, but hardest to use.  It simply deals with serialization and deserialization of MQTT packets.  Serialization means taking application data and converting it to a form ready for sending across the network.  Deserialization means taking the data read from the network and extracting the data.
+</li>
+<li>MQTTClient.  This is a C++ library first written for mbed, but now ported to other platforms.  Although it uses C++, it still avoids dynamic memory allocations, and has replaceable classes for OS and network dependent functions.  Use of the STL is also avoided.  It is based on, and requires, MQTTPacket.
+</li>
+<li>MQTTClient-C.  A C version of MQTTClient, for environments where C++ is not the norm, such as FreeRTOS.  Also built on top of MQTTPacket.
+</li>
+</ul>
 </p>
 
 <h2 id="source">Source</h2>
@@ -21,25 +32,118 @@ Posix or Windows libraries for networking (sockets), threads and memory allocati
 
 <h2 id="download">Download</h2>
 
-<p>A prebuilt Arduino port of this client is available from the <a href="https://projects.eclipse.org/projects/technology.paho/downloads">Paho downloads page</a>. To use download and in the Arduino IDE use Sketch -> Import Library... -> Add Library... with the downloaded client zip file.  An example Arduino sketch demonstrating the client is included.</p>
+<p>A prebuilt Arduino port of MQTTClient is available from the <a href="https://projects.eclipse.org/projects/technology.paho/downloads">Paho downloads page</a>. To use download and in the Arduino IDE use Sketch -> Import Library... -> Add Library... with the downloaded client zip file.  An example Arduino sketch demonstrating the client is included.</p>
 
 <h2 id="build-from-source">Building from source</h2>
 
 <h3>Gcc</h3>
 
-<p>Samples and tests can be built with "build" shell scripts in their respective directories</p>
+<p>Samples and tests can be built with "build" shell scripts in their respective directories.  Makefiles are being worked on.</p>
 
 <h2 id="documentation">Documentation</h2>
 
-<p><a href="http://modelbasedtesting.co.uk/?p=69">New “Embedded” Paho MQTT C Client</a></p>
+<p>MQTTPacket: <a href="http://modelbasedtesting.co.uk/?p=69">New “Embedded” Paho MQTT C Client</a></p>
 
-<p><a href="http://modelbasedtesting.co.uk/?p=79">Receiving messages with the Paho embedded C client</a></p>
+<p>MQTTPacket: <a href="http://modelbasedtesting.co.uk/?p=79">Receiving messages with the Paho embedded C client</a></p>
+
+<p>MQTTClient: <a href="http://modelbasedtesting.co.uk/?p=171">Paho embedded C++ client on mbed and for Arduino</a></p>
+
+<p>MQTTClient: <a href="http://modelbasedtesting.co.uk/?p=181">Using TLS with the the Paho embedded C++ client</a></p>
+
+<p>MQTTClient: <a href="http://modelbasedtesting.co.uk/?p=131">Porting the Paho synchronous embedded C++ client</a></p>
 
 <h3 id="getting-started">Getting Started</h3>
 
-<p>Here is the core of a simple publishing program:<p>
+<h4>MQTTClient</h3>
 
-<pre>
+<p>Here is a simple publishing and subscribing program for the MQTTClient library on Linux:<p>
+
+<code><pre>
+#define MQTTCLIENT_QOS2 1
+
+#include "MQTTClient.h"
+
+#define DEFAULT_STACK_SIZE -1
+
+#include "linux.cpp"
+
+int arrivedcount = 0;
+
+void messageArrived(MQTT::MessageData& md)
+{
+    MQTT::Message &message = md.message;
+
+	printf("Message %d arrived: qos %d, retained %d, dup %d, packetid %d\n", 
+		++arrivedcount, message.qos, message.retained, message.dup, message.id);
+    printf("Payload %.*s\n", (int)message.payloadlen, (char*)message.payload);
+}
+
+
+int main(int argc, char* argv[])
+{   
+    IPStack ipstack = IPStack();
+    float version = 0.3;
+    const char* topic = "mbed-sample";
+    
+    printf("Version is %f\n", version);
+              
+    MQTT::Client<IPStack, Countdown> client = MQTT::Client<IPStack, Countdown>(ipstack);
+    
+    const char* hostname = "iot.eclipse.org";
+    int port = 1883;
+    printf("Connecting to %s:%d\n", hostname, port);
+    int rc = ipstack.connect(hostname, port);
+	if (rc != 0)
+	    printf("rc from TCP connect is %d\n", rc);
+ 
+	printf("MQTT connecting\n");
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;       
+    data.MQTTVersion = 3;
+    data.clientID.cstring = (char*)"mbed-icraggs";
+    rc = client.connect(data);
+	if (rc != 0)
+	    printf("rc from MQTT connect is %d\n", rc);
+	printf("MQTT connected\n");
+    
+    rc = client.subscribe(topic, MQTT::QOS2, messageArrived);   
+    if (rc != 0)
+        printf("rc from MQTT subscribe is %d\n", rc);
+
+    MQTT::Message message;
+
+    // QoS 0
+    char buf[100];
+    sprintf(buf, "Hello World!  QoS 0 message from app version %f", version);
+    message.qos = MQTT::QOS0;
+    message.retained = false;
+    message.dup = false;
+    message.payload = (void*)buf;
+    message.payloadlen = strlen(buf)+1;
+    rc = client.publish(topic, message);
+	if (rc != 0)
+		printf("Error %d from sending QoS 0 message\n", rc);
+    else while (arrivedcount == 0)
+        client.yield(100);
+        
+    rc = client.unsubscribe(topic);
+    if (rc != 0)
+        printf("rc from unsubscribe was %d\n", rc);
+    
+    rc = client.disconnect();
+    if (rc != 0)
+        printf("rc from disconnect was %d\n", rc);
+    
+    ipstack.disconnect();
+    
+    return 0;
+}
+</pre></code>
+
+<h4>MQTTPacket</h3>
+
+<p>Here is the core of a simple publishing program for the MQTTPacket library:<p>
+
+<code><pre>
 MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
 int rc = 0;
 char buf[200];
@@ -60,6 +164,6 @@ len += MQTTSerialize_disconnect(buf + len, buflen - len); /* 3 */
 rc = Socket_new("127.0.0.1", 1883, &mysock);
 rc = write(mysock, buf, len);
 rc = close(mysock);
-</pre>
+</pre></code>
 <?php include '../../../_includes/footer.php' ?>
 

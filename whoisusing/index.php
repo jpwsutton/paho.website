488a6251ee9c021e9cdf307b42e13d58db97c1af
Ian Craggs <icraggs@uk.ibm.com>
2015-09-18 12:06:59 +0100

    Add who is using page

---
+++ whoisusing/index.php
@@ -0,0 +1,16 @@
+<?php include '../../_includes/header.php' ?>
+
+<h1>Who is using Paho?</h1>
+
+<h2>Lots of organizations are using Eclipse Paho for their MQTT client needs.
+Check out our recent user stories.</h2>
+
+<p>If you are using Paho, please tell us your story by sending an email to: icraggs@uk.ibm.com.  Thanks!</p>
+
+<h2>COMPANY NAME GOES HERE</h2>
+<p>John Doe, President</p>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia id quam a eleifend. Aliquam erat volutpat. Quisque tincidunt bibendum auctor. Praesent id pharetra risus, eu sodales sapien.</p>
+
+<?php include '../../_includes/footer.php' ?>
+

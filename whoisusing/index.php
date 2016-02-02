167b7df9befea45fd7d23e3b519d43fc83201ff0
Ian Craggs <icraggs@uk.ibm.com>
2016-02-02 21:21:30 +0000

    Add first story

--- whoisusing/index.php
+++ whoisusing/index.php
@@ -7,10 +7,10 @@ Check out our recent user stories.</h2>
 
 <p>If you are using Paho, please tell us your story by sending an email to: paho-success@eclipse.org.  Thanks!</p>
 
-<h2>COMPANY NAME GOES HERE</h2>
-<p>John Doe, President</p>
+<h2>Universidad de Castilla-La Mancha</h2>
+<p>José Luis Espinosa Aranda, Ph. D., Assistant Researcher</p>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacinia id quam a eleifend. Aliquam erat volutpat. Quisque tincidunt bibendum auctor. Praesent id pharetra risus, eu sodales sapien.</p>
+<p>Currently we are working in the Eyes of Things project funded by the <a href="http://eyesofthings.eu">Horizon 2020 program of the EU</a>. In this project we are using Paho and the MQTT protocol as a way to control and configure the device we are working on.  You could read more in this <a href="http://eyesofthings.eu/wp-content/uploads/2015/08/SPC15.pdf">paper</a>.</p>
 
 <?php include '../../_includes/footer.php' ?>
 

0ae0cc284de5ee6ffc6688fc5e5c8a41ea2d7ef6
Ian Craggs <icraggs@uk.ibm.com>
2016-02-18 14:46:44 +0000

    Add tweets

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
                         </p>
                     </div>
                     <div class="span5">
+                    <a class="twitter-timeline" href="https://twitter.com/eclipsepaho" data-widget-id="700329712393048064">Tweets by @eclipsepaho</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+                    </div>
+                    <div class="span5">
                     <p></p>
 <table class="table table-condensed">
 <tr><th style="padding: 10px 0; font-size: 1.1em;">Latest release: <b>1.1</b></th><th style="padding: 10px 0; font-size: 1.1em;"><a href="https://projects.eclipse.org/projects/technology.paho/downloads">Downloads</a></th></tr>
